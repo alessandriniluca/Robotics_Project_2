401b37c6b364da76271b5b78fbd61dbe4a7e5b24
mattia1p <mattiapor@gmail.com>
2022-06-30 23:09:46 +0200

    fix1

--- src/map_server/src/map_saver_trajectory.cpp
+++ src/map_server/src/map_saver_trajectory.cpp
@@ -127,24 +127,26 @@ class MapTrajectory
       int y_init = 0;
       for(unsigned int i = 0; i<trajectory.size();i++){
         int pixel_position = static_cast<int>(trajectory[i].pose.pose.position.x/map_saved.info.resolution + map_saved.info.width/2) + (map_saved.info.height - static_cast<int>(trajectory[i].pose.pose.position.y/map_saved.info.resolution + map_saved.info.height/2) - 1) * map_saved.info.width;
-        map_saved.data[pixel_position] = -2;
+        // map_saved.data[pixel_position] = -2;
         if(i!=0){
           int pixel_position_prev_i = static_cast<int>(trajectory[i-1].pose.pose.position.x/map_saved.info.resolution + map_saved.info.width/2) + (map_saved.info.height - static_cast<int>(trajectory[i-1].pose.pose.position.y/map_saved.info.resolution + map_saved.info.height/2) - 1) * map_saved.info.width;
           int x0 = pixel_position_prev_i % map_saved.info.width;
           int y0 = pixel_position_prev_i / map_saved.info.width;
           int x1 = pixel_position % map_saved.info.width;
           int y1 = pixel_position / map_saved.info.width;
+          map_saved.data[x0 + (map_saved.info.height - y0 - 1) * map_saved.info.width] = -2;
+          // int x0 = trajectory[i-1].pose.pose.position.x/map_saved.info.resolution + map_saved.info.width/2;
+          // int y0 = trajectory[i-1].pose.pose.position.y/map_saved.info.resolution + map_saved.info.height/2;
+          // int x1 = trajectory[i].pose.pose.position.x/map_saved.info.resolution + map_saved.info.width/2;
+          // int y1 = trajectory[i].pose.pose.position.y/map_saved.info.resolution + map_saved.info.height/2;
           interpolate(x0, y0, x1, y1);
           for (int i = 0; i<x_interpolation.size(); i++){
-            if (y_init == 0){
-              y_init = y_interpolation[0];
-            }
-            int pixel_position_temp = x_interpolation[i] + (map_saved.info.height - y_interpolation[i] + 2*(y_interpolation[i]-y_init) - 2) * map_saved.info.width;
+            int pixel_position_temp = x_interpolation[i] + (map_saved.info.height - y_interpolation[i] - 1) * map_saved.info.width;
             map_saved.data[pixel_position_temp] = -3;
           }
         }
       }
-      ROS_INFO("Generating map file ...")
+      ROS_INFO("Generating map file ...");
       fprintf(out, "P5\n# CREATOR: map_saver_trajectory.cpp %.3f m/pix\n%d %d\n255\n",
               map_saved.info.resolution, map_saved.info.width, map_saved.info.height);
       for(unsigned int y = 0; y < map_saved.info.height; y++) {
